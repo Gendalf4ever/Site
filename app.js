40e83428d125d6d8f98bb815c6c9f1b620512201
master <chelovek438@gmail.com>
2022-03-14 08:16:10 +0300

    translation scripts added. Added english translation

---
+++ app.js
@@ -0,0 +1,34 @@
+
+const select = document.querySelector('select');
+const allLang = ['ru', 'en', 'lit'];
+
+select.addEventListener('change', changeURLLanguage);
+
+// перенаправить на url с указанием языка
+function changeURLLanguage() {
+    let lang = select.value;
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+}
+
+function changeLanguage() {
+    let hash = window.location.hash;
+    hash = hash.substring(1);
+    console.log(hash);
+    if (!allLang.includes(hash)) {
+        location.href = window.location.pathname + '#en';
+        location.reload();
+    }
+    select.value = hash;
+   //document.querySelector('title').innerHTML = langArr['main'][hash];
+    // document.querySelector('.lng-project').innerHTML = langArr['project'][hash];
+    for (let key in langArr) {
+        let elem = document.querySelector('.lng-' + key);
+        if (elem) {
+            elem.innerHTML = langArr[key][hash];
+        }
+
+    }
+}
+
+changeLanguage();
---
+++ lang.js
@@ -0,0 +1,267 @@
+const langArr = {
+    "main" :  {
+        "ru": "Главная",
+        "en": "Main",
+        "lit": "buba",
+    }, 
+    "project": {
+        "ru": "О проекте и партнерах",
+        "en": "About",
+        "lit": "графічний чіп",
+    }, 
+    "results": {
+        "ru": "Результаты",
+        "en": "Results",
+        "lit": "oбсяг пам'яті",
+    }, 
+    "contacts": {
+        "ru": "Контакты",
+        "en": "Contacts",
+        "lit": "nип пам'яті",
+    }, 
+    "arButton":{
+        "ru": "Тест устройства",
+        "en": "Device test",
+        "lit": "nип пам'яті"
+    },
+    "brahert": {
+        "ru": "Скульптор Герман Брахерт",
+        "en": "German Brahert",
+        "lit": "nип пам'яті"
+    },
+    "brahert-description": {
+        "ru": "Дом-Музей Брахерта",
+        "en": "German Brahert Museum",
+        "lit": "nип пам'яті"
+    },
+    "genius": {
+        "ru": "Путешествие гениев",
+        "en": "Genius travel",
+        "lit": "nип пам'яті"
+    },
+    "genius-text":{
+        "ru": "Мы предлагаем Вам поучаствовать в интересных интерактивных квестах, созданных специально для музеев в Доме-музее Германа Брахерта, открытом в 1993 году в поселке Отрадное Светлогорского городского округа, музее Видунаса, в музее Иммануила Канта, расположенном в Кафедральном соборе в городе Кенингсберг(он же Калининград), там же расположено и мемориальное захоронение И. Канта, и в Мемориальном музее писателя Томаса Манна - одном из самых посещаемых музеев Западной Литвы, находящемся в Ниде на горе Уошвес.",
+        "en": "Genius travel",
+        "lit": "nип пам'яті"
+    },
+    "vidunas": {
+        "ru": "Философ Вильгемас Стороста",
+        "en": "Philosopher Vilhemas Storosta",
+        "lit": "nип пам'яті"
+    },
+    "vidunas-description": {
+        "ru": "Культурный центр Видунаса города Кинтай",
+        "en": "Vidunas culture center",
+        "lit": "nип пам'яті"
+    },
+    "kant" : {
+        "ru": "Философ И.Кант",
+        "en": "Philosopher I.Kant",
+        "lit": "nип пам'яті"
+    },
+    "kant-description": {
+        "ru": "Кафедральный собор",
+        "en": "Cathedral",
+        "lit": "nип пам'яті"
+    },
+    "thomas": {
+        "ru": "Писатель Томас Манн",
+        "en": "Writer Thomas Mann",
+        "lit": "nип пам'яті"
+    },
+    "thomas-description": {
+        "ru": "Музей Неринги",
+        "en": "Neringa Museum",
+        "lit": "nип пам'яті"
+    },
+    "registrationButton": {
+        "ru": "Регистрация",
+        "en": "Registration",
+        "lit": "nип пам'яті"
+    },
+    "registrationText": {
+        "ru": "Регистрация",
+        "en": "Registration",
+        "lit": "nип пам'яті"
+    },
+    "nicknameLabel": {
+        "ru": "Имя пользователя:",
+        "en": "Nickname:",
+        "lit": "nип пам'яті"
+    },
+    "emailLabel": {
+        "ru": "Электронная почта:",
+        "en": "Email:",
+        "lit": "nип пам'яті"
+    },
+    "passwordLabel": {
+        "ru": "Пароль:",
+        "en": "Password:",
+        "lit": "nип пам'яті"
+    },
+    "confirmPasswordLabel":{
+        "ru": "Подтвердите пароль:",
+        "en": "Confirm password:",
+        "lit": "nип пам'яті"
+    },
+    "your-points":{
+        "ru": "Ваши баллы",
+        "en": "Your points",
+        "lit": "nип пам'яті"  
+    },
+    "your-name":{
+        "ru": "Имя:",
+        "en": "Name:",
+        "lit": "nип пам'яті"  
+    },
+    "points":{
+        "ru": "Баллы:",
+        "en": "Points:",
+        "lit": "nип пам'яті" 
+    },
+    "time":{
+        "ru": "Время:",
+        "en": "Time:",
+        "lit": "nип пам'яті" 
+    },
+    "members": {
+        "ru": "Участники",
+        "en": "Members",
+        "lit": "nип пам'яті" 
+    },
+    "other-name": {
+        "ru": "Имя:",
+        "en": "Name:",
+        "lit": "nип пам'яті" 
+    },
+    "other-points":{
+        "ru": "Баллы:",
+        "en": "Points:",
+        "lit": "nип пам'яті" 
+    },
+    "other-time": {
+        "ru": "Время:",
+        "en": "Time:",
+        "lit": "nип пам'яті" 
+    },
+    "review": {
+        "ru": "Отзывы:",
+        "en": "Review:",
+        "lit": "nип пам'яті" 
+    },
+    "loginButton":{
+        "ru": "Войти",
+        "en": "Login",
+        "lit": "nип пам'яті" 
+    },
+    "contactUs":{
+        "ru": "Связаться с нами",
+        "en": "Contact us",
+        "lit": "nип пам'яті" 
+    },
+    "chapter":{
+        "ru": "ЛИТОВСКО-РОССИЙСКАЯ ПРОГРАММА ПРИГРАНИЧНОГО СОТРУДНИЧЕСТВА 2014-2020",
+        "en": "LITHUANIA-RUSSIA CROSS-BORDER COOPERATION PROGRAMME 2014-2020",
+        "lit": "nип пам'яті" 
+    },
+    "description-text":{
+        "ru": "С 2007 года Российская Федерация участвует в Программах приграничного сотрудничества Россия-ЕС на равных и взаимовыгодных условиях. Финансовый вклад Российской Федерации в Программы приграничного сотрудничества обеспечивается Федеральным бюджетом РФ. Сотрудничество с Литвой, Польшей, Латвией, Эстонией, Финляндией, Швецией и Норвегией в рамках Программ приграничного сотрудничества  стало эффективным инструментом, обеспечивающим рост благосостояния региона. Меры, предпринятые в рамках Программ приграничного сотрудничества, соответствуют и согласовываются с национальными и региональными документами социально-экономического развития и стратегического планирования Российской Федерации",
+        "en": "The Russian Federation participates in Russia – EU Cross Border Cooperation Programmes on equal and mutually beneficial base since 2007 The financial contribution of the Russian Federation to the CBC Programmes is provided under the Federal Budget of the Russian Federation. The cooperation with Lithuania, Poland, Latvia, Estonia, Finland, Sweden and Norway within the CBC Programmes has proved to be an effective instrument to boost the prosperity of the region. The measures undertaken within the CBC Programmes are in line and correspond to national and regional documents of social and economic development and strategic planning of the Russian Federation.",
+        "lit": "nип пам'яті" 
+    },
+    "program-website-text":{
+        "ru": "Сайт программы:",
+        "en": " Program website:",
+        "lit": "nип пам'яті" 
+    },
+    "project-description":{
+        "ru": "Название проекта: Гении места в диалоге и движении: создание пространства для развития туризма",
+        "en": "Title of the Project: Genius Loci in dialogue and motion: creating space for tourism development",
+        "lit": "nип пам'яті"
+    },
+    "project-realization":{
+        "ru": "Проект реализуется четырьмя организациями, сохраняющими всемирно известное наследие знаковых для нашего региона персон, которые жили и работали на территории Программы: философа Иммануила Канта, лауреата Нобелевской премии писателя Томаса Манна, скульптора Германа Брахерта, писателя и философа Видунаса:",
+        "en": "The project is being implemented by four organizations that preserve the world-famous heritage of people who were iconic for our region who lived and worked in the Program area: the philosopher Immanuel Kant, the Nobel Prize laureate writer Thomas Mann, the sculptor Hermann Brachert, the writer and philosopher Vidunas:",
+        "lit": "nип пам'яті"
+    },
+    "rfLocation":{
+        "ru": "Российская Федерация:",
+        "en": "Russian Federation:",
+        "lit": "nип пам'яті" 
+    },
+    "litvaLocation":{
+        "ru": "Литовская республика:",
+        "en": "Republic of Lithuania:",
+        "lit": "nип пам'яті" 
+    },
+    "firstMuseumRF":{
+        "ru": "Кафедральный собор (Калининградская область)",
+        "en": "Cathedral (Kaliningrad region)",
+        "lit": "nип пам'яті" 
+    },
+    "secondMuseumRF":{
+        "ru": "Дом-музей Германа Брахерта (Калининградская область)",
+        "en": "House-Museum of Herman Brachert (Kaliningrad region)",
+        "lit": "nип пам'яті" 
+    },
+    "firstMuseumLitva":{
+        "ru": "Музеи Неринги (город Нида)",
+        "en": "Museums of Neringa (Nida)",
+        "lit": "nип пам'яті" 
+    },
+    "secondMuseumLitva":{
+        "ru": "Культурный центр Видунаса (город Кинтай)",
+        "en": "Vidunas Cultural Center (Kintai)",
+        "lit": "nип пам'яті" 
+    },
+    "financial" : {
+        "ru": " Проект финансируется Европейским Союзом и Российской Федерацией. Содержаниеопубликованного материала является предметом ответственности ГАУ КО «Кафедральный собор» и не отражает точку зрения Европейского Союза и Российской Федерации",
+        "en": "This project is funded by the European Union and the Russian Federation. The contents of this publication are the sole responsibility of <name of the Beneficiary> and can in no way be taken to reflect the views of the European Union.",
+        "lit": "nип пам'яті"
+    },
+    "results-text": {
+        "ru": "Основные результаты проекта:",
+        "en":"The main results of the project:",
+        "lit":""
+    },
+    "result-one":{
+        "ru": "- новые музейные экспозиции в музее Иммануила Канта в Кафедральном соборе в Калининграде и в музее Томаса Манна в Ниде",
+        "en": "Login",
+        "lit": "nип пам'яті" 
+    },
+    "result-two":{
+        "ru": "- мультимедийные стенды для сэлфи с И. Кантом и Т. Манном",
+        "en": "Login",
+        "lit": "nип пам'яті" 
+    },
+    "result-three":{
+        "ru": "- интернет-игра-квест «Путешествие Гениев»",
+        "en": "Login",
+        "lit": "nип пам'яті" 
+    },
+    "result-four":{
+        "ru": "- российско-литовский фестиваль «Гений Света- Гений просвещения» на территории поселков Отрадное и Веселовка в Калининградской области России, в Ниде и Кинтае Литовской Республики",
+        "en": "Login",
+        "lit": "nип пам'яті" 
+    },
+    "result-five":{
+        "ru": "- музыкальная скамейка и образовательная световая «тропа Видунаса» в Кинтае",
+        "en": "Login",
+        "lit": "nип пам'яті" 
+    },
+    "result-six":{
+        "ru": "- тактильная модель знаменитой скульптуры Г. Брахерта «Несущая воду» в поселке Отрадное",
+        "en": "Login",
+        "lit": "nип пам'яті" 
+    },
+    "result-seven":{
+        "ru": "- уличные системы для зарядки мобильных телефонов рядом со всеми учреждениями",
+        "en": "Login",
+        "lit": "nип пам'яті" 
+    },
+    "result-eight":{
+        "ru": "- повышение технической оснащенности деятельности участников проекта",
+        "en": "Login",
+        "lit": "nип пам'яті" 
+    },
+}
