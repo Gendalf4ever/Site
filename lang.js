4482a7f3711974d8d6ce2f5e68e2c9756fd1e9bc
master <chelovek438@gmail.com>
2022-03-15 10:33:50 +0300

    some test

--- lang.js
+++ lang.js
@@ -40,8 +40,8 @@ const langArr = {
         "lit": "nип пам'яті"
     },
     "genius-text":{
-        "ru": "Мы предлагаем Вам поучаствовать в интересных интерактивных квестах, созданных специально для музеев в Доме-музее Германа Брахерта, открытом в 1993 году в поселке Отрадное Светлогорского городского округа, музее Видунаса, в музее Иммануила Канта, расположенном в Кафедральном соборе в городе Кенингсберг(он же Калининград), там же расположено и мемориальное захоронение И. Канта, и в Мемориальном музее писателя Томаса Манна - одном из самых посещаемых музеев Западной Литвы, находящемся в Ниде на горе Уошвес.",
-        "en": "Genius travel",
+        "ru": "Мы предлагаем Вам поучаствовать в интересных интерактивных квестах, созданных специально для 4 музеев. В Доме-музее Германа Брахерта, открытом в 1993 году в поселке Отрадное Светлогорского городского округа,  в музее Видунаса, в музее Иммануила Канта, расположенном в Кафедральном соборе в городе Кенингсберг(он же Калининград), там же расположено и мемориальное захоронение И. Канта, и в Мемориальном музее писателя Томаса Манна - одном из самых посещаемых музеев Западной Литвы, находящемся в Ниде на горе Уошвес.",
+        "en": "We are offering you to be a part of interesting interactive quests, which were created special for: museum of German Brahert, opened in 1993 in Otradnoye, Svetlogorskiy county, Vidunas museum, Emmanuel Kant museum, located in Cathedral in Koenigsberg(Kaliningrad). In this place is also located memorial burial place of E.Kant, and in Memorial museum of writer Thomas Mann - one of the most visited museums in West Lithuania, located in Nida, mountain Uoshwes",
         "lit": "nип пам'яті"
     },
     "vidunas": {
@@ -162,106 +162,107 @@ const langArr = {
     "chapter":{
         "ru": "ЛИТОВСКО-РОССИЙСКАЯ ПРОГРАММА ПРИГРАНИЧНОГО СОТРУДНИЧЕСТВА 2014-2020",
         "en": "LITHUANIA-RUSSIA CROSS-BORDER COOPERATION PROGRAMME 2014-2020",
-        "lit": "nип пам'яті" 
+        "lit": "2014–2020 M. LIETUVOS IR RUSIJOS BENDRADARBIAVIMO PER SIENĄ PROGRAMA" 
     },
     "description-text":{
         "ru": "С 2007 года Российская Федерация участвует в Программах приграничного сотрудничества Россия-ЕС на равных и взаимовыгодных условиях. Финансовый вклад Российской Федерации в Программы приграничного сотрудничества обеспечивается Федеральным бюджетом РФ. Сотрудничество с Литвой, Польшей, Латвией, Эстонией, Финляндией, Швецией и Норвегией в рамках Программ приграничного сотрудничества  стало эффективным инструментом, обеспечивающим рост благосостояния региона. Меры, предпринятые в рамках Программ приграничного сотрудничества, соответствуют и согласовываются с национальными и региональными документами социально-экономического развития и стратегического планирования Российской Федерации",
         "en": "The Russian Federation participates in Russia – EU Cross Border Cooperation Programmes on equal and mutually beneficial base since 2007 The financial contribution of the Russian Federation to the CBC Programmes is provided under the Federal Budget of the Russian Federation. The cooperation with Lithuania, Poland, Latvia, Estonia, Finland, Sweden and Norway within the CBC Programmes has proved to be an effective instrument to boost the prosperity of the region. The measures undertaken within the CBC Programmes are in line and correspond to national and regional documents of social and economic development and strategic planning of the Russian Federation.",
-        "lit": "nип пам'яті" 
+        "lit": "Rusijos Federacija dalyvauja Rusijos ir ES tarpvalstybinio bendradarbiavimo per sieną programose vienodomis ir abipusiai naudingomis sąlygomis nuo 2007 metų. Finansinis Rusijos Federacijos įnašas į bendradarbiavimo per sieną (BPS) programas teikiamas iš Rusijos Federacijos federacinio biudžeto. Bendradarbiavimas su Lietuva, Lenkija, Latvija, Estija, Suomija, Švedija ir Norvegija pagal BPS programas pasirodė esąs veiksmingas regiono klestėjimo skatinimo instrumentas. Priemonės, įgyvendinamos pagal BPS programas, atitinka ir atliepia nacionalinius ir regioninius Rusijos Federacijos socialinės ir ekonominės plėtros ir strateginio planavimo dokumentus." 
     },
     "program-website-text":{
         "ru": "Сайт программы:",
         "en": " Program website:",
-        "lit": "nип пам'яті" 
+        "lit": "Programos svetainė:" 
     },
     "project-description":{
         "ru": "Название проекта: Гении места в диалоге и движении: создание пространства для развития туризма",
         "en": "Title of the Project: Genius Loci in dialogue and motion: creating space for tourism development",
-        "lit": "nип пам'яті"
+        "lit": "Projekto pavadinimas: Vietos genijai dialoge ir judėjime: erdvės kūrimas turizmo plėtrai"
     },
     "project-realization":{
         "ru": "Проект реализуется четырьмя организациями, сохраняющими всемирно известное наследие знаковых для нашего региона персон, которые жили и работали на территории Программы: философа Иммануила Канта, лауреата Нобелевской премии писателя Томаса Манна, скульптора Германа Брахерта, писателя и философа Видунаса:",
         "en": "The project is being implemented by four organizations that preserve the world-famous heritage of people who were iconic for our region who lived and worked in the Program area: the philosopher Immanuel Kant, the Nobel Prize laureate writer Thomas Mann, the sculptor Hermann Brachert, the writer and philosopher Vidunas:",
-        "lit": "nип пам'яті"
+        "lit": "Projektą įgyvendina keturios organizacijos, saugančios pasaulinio garso mūsų regionui žymių žmonių, gyvenusių ir dirbusių Programos teritorijoje, paveldą: filosofas Immanuelis Kantas, Nobelio premijos laureatas rašytojas Thomas Mannas, skulptorius Hermannas Brachertas, rašytojas ir filosofas Vidūnas:"
     },
     "rfLocation":{
         "ru": "Российская Федерация:",
         "en": "Russian Federation:",
-        "lit": "nип пам'яті" 
+        "lit": "Rusijos Federacija:" 
     },
     "litvaLocation":{
         "ru": "Литовская республика:",
         "en": "Republic of Lithuania:",
-        "lit": "nип пам'яті" 
+        "lit": "Lietuvos Respublika:" 
     },
     "firstMuseumRF":{
         "ru": "Кафедральный собор (Калининградская область)",
         "en": "Cathedral (Kaliningrad region)",
-        "lit": "nип пам'яті" 
+        "lit": "Katedra (Kaliningrado sritis)" 
     },
     "secondMuseumRF":{
         "ru": "Дом-музей Германа Брахерта (Калининградская область)",
         "en": "House-Museum of Herman Brachert (Kaliningrad region)",
-        "lit": "nип пам'яті" 
+        "lit": "Hermano Bracherto namas-muziejus (Kaliningrado sritis)" 
     },
     "firstMuseumLitva":{
         "ru": "Музеи Неринги (город Нида)",
         "en": "Museums of Neringa (Nida)",
-        "lit": "nип пам'яті" 
+        "lit": "Neringos muziejai (Nidа)" 
     },
     "secondMuseumLitva":{
         "ru": "Культурный центр Видунаса (город Кинтай)",
         "en": "Vidunas Cultural Center (Kintai)",
-        "lit": "nип пам'яті" 
+        "lit": "Viduno kultūros centras (Kintų)" 
+    },
+  
+    "results-text": {
+        "ru": "Основные результаты проекта:",
+        "en":"The main results of the project:",
+        "lit":"Pagrindiniai projekto rezultatai:"
+    },
+    "result-one":{
+        "ru": "- новые музейные экспозиции в музее Иммануила Канта в Кафедральном соборе в Калининграде и в музее Томаса Манна в Ниде",
+        "en": "- new museum expositions in the Immanuel Kant Museum in the Cathedral in Kaliningrad and in the Thomas Mann Museum in Nida",
+        "lit": "- naujos muziejinės ekspozicijos Immanuelio Kanto muziejuje Kaliningrado katedroje ir Tomo Mano muziejuje Nidoje" 
+    },
+    "result-two":{
+        "ru": "- мультимедийные стенды для сэлфи с И. Кантом и Т. Манном",
+        "en": "- multimedia stands for selfies with I. Kant and T. Mann",
+        "lit": "- multimedijos stendai asmenukėms su I. Kantu ir T. Mannu" 
+    },
+    "result-three":{
+        "ru": "- интернет-игра-квест «Путешествие Гениев»",
+        "en": "- Internet game-quest «Journey of Geniuses»",
+        "lit": "- Interneto žaidimas «Genių kelionė»" 
+    },
+    "result-four":{
+        "ru": "- российско-литовский фестиваль «Гений Света- Гений просвещения» на территории поселков Отрадное и Веселовка в Калининградской области России, в Ниде и Кинтае Литовской Республики",
+        "en": "- Russian-Lithuanian festival «Genius of Light - Genius of Enlightenment» on the territory of the villages Otradnoye and Veselovka in the Kaliningrad region of Russia, in Nida and Kintai of the Republic of Lithuania",
+        "lit": "n- Rusijos ir lietuvių festivalis «Šviesos genijus – Apšvietos genijus» Rusijos Kaliningrado srities Otradnoje ir Veselovkos kaimų teritorijoje, Lietuvos Respublikos Nidoje ir Kintuose.і" 
+    },
+    "result-five":{
+        "ru": "- музыкальная скамейка и образовательная световая «тропа Видунаса» в Кинтае",
+        "en": "- music bench and educational light «Vidunas trail» in Kintai",
+        "lit": "- muzikinis suoliukas ir edukacinė šviesa «Vidūno takas» Kintuose" 
+    },
+    "result-six":{
+        "ru": "- тактильная модель знаменитой скульптуры Г. Брахерта «Несущая воду» в поселке Отрадное",
+        "en": "- a tactile model of the famous sculpture by G. Brachert «Carrying Water» in the village of Otradnoye",
+        "lit": "- žymiosios G. Bracherto skulptūros «Nešantis vandenį» Otradnoje kaime taktilinis maketas." 
+    },
+    "result-seven":{
+        "ru": "- уличные системы для зарядки мобильных телефонов рядом со всеми учреждениями",
+        "en": "- outdoor charging systems for mobile phones close to all establishments",
+        "lit": "- netoli visų įstaigų lauko mobiliųjų telefonų įkrovimo sistemos" 
+    },
+    "result-eight":{
+        "ru": "- повышение технической оснащенности деятельности участников проекта",
+        "en": "- increasing the technical equipment of the activities of the project participants",
+        "lit": "- projekto dalyvių veiklos techninio aprūpinimo didinimas" 
     },
     "financial" : {
         "ru": " Проект финансируется Европейским Союзом и Российской Федерацией. Содержаниеопубликованного материала является предметом ответственности ГАУ КО «Кафедральный собор» и не отражает точку зрения Европейского Союза и Российской Федерации",
         "en": "This project is funded by the European Union and the Russian Federation. The contents of this publication are the sole responsibility of <name of the Beneficiary> and can in no way be taken to reflect the views of the European Union.",
-        "lit": "nип пам'яті"
-    },
-    "results-text": {
-        "ru": "Основные результаты проекта:",
-        "en":"The main results of the project:",
-        "lit":""
-    },
-    "result-one":{
-        "ru": "- новые музейные экспозиции в музее Иммануила Канта в Кафедральном соборе в Калининграде и в музее Томаса Манна в Ниде",
-        "en": "Login",
-        "lit": "nип пам'яті" 
-    },
-    "result-two":{
-        "ru": "- мультимедийные стенды для сэлфи с И. Кантом и Т. Манном",
-        "en": "Login",
-        "lit": "nип пам'яті" 
-    },
-    "result-three":{
-        "ru": "- интернет-игра-квест «Путешествие Гениев»",
-        "en": "Login",
-        "lit": "nип пам'яті" 
-    },
-    "result-four":{
-        "ru": "- российско-литовский фестиваль «Гений Света- Гений просвещения» на территории поселков Отрадное и Веселовка в Калининградской области России, в Ниде и Кинтае Литовской Республики",
-        "en": "Login",
-        "lit": "nип пам'яті" 
-    },
-    "result-five":{
-        "ru": "- музыкальная скамейка и образовательная световая «тропа Видунаса» в Кинтае",
-        "en": "Login",
-        "lit": "nип пам'яті" 
-    },
-    "result-six":{
-        "ru": "- тактильная модель знаменитой скульптуры Г. Брахерта «Несущая воду» в поселке Отрадное",
-        "en": "Login",
-        "lit": "nип пам'яті" 
-    },
-    "result-seven":{
-        "ru": "- уличные системы для зарядки мобильных телефонов рядом со всеми учреждениями",
-        "en": "Login",
-        "lit": "nип пам'яті" 
-    },
-    "result-eight":{
-        "ru": "- повышение технической оснащенности деятельности участников проекта",
-        "en": "Login",
-        "lit": "nип пам'яті" 
+        "lit": "Šį projektą finansuoja Europos Sąjunga ir Rusijos Federacija. Už publikacijos turinį atsako tik VAO KS 'Katedra' ir jis negali būti laikomas atspindinčiu Europos Sąjungos požiūrį ir Rusijos Federacija"
     },
 }
